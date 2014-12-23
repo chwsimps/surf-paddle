78e779364ca27baed6f7a6dd60f0fc41746fe8d3
Chase Simpson <chase.w.simpson1@gmail.com>
2014-12-23 09:59:37 -0500

    gulp deploy test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-  // place code for your default task here
+
+  gulp.task('deploy', function() {
+
+    gulp.src('/')
+      .pipe(prompt.prompt({
+          type: 'confirm',
+          name: 'task',
+          message: 'This will deploy to GitHub Pages. Have you already built your application and pushed your updated master branch?'
+      }, function(res){
+        if (res.task){
+          console.log('Attempting: "git subtree push --prefix dist origin gh-pages"');
+          exec('git subtree push --prefix dist origin gh-pages', function(err, stdout, stderr) {
+              console.log(stdout);
+              console.log(stderr);
+          });
+        } else { console.log('Please do this first and then run `gulp deploy` again.'); }
+      }));
+
+  });
+
 });
